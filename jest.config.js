0f81441a76e2932b5265b3866597e770bc419df5
JooduG <62556217+JooduG@users.noreply.github.com>
2025-09-09 19:44:33 +0200

    prog

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,12 @@
 /** @type {import('jest').Config} */
 const config = {
   testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/tests/setup-jest.js'],
+  // Correcting the path to the setup file. It needs to go up two directories
+  // from the config folder to find the root, then go into the tests folder.
+  setupFilesAfterEnv: ['<rootDir>/../../tests/setup-jest.js'],
   reporters: [
     'default'
-  ],
-  transform: {
-    '^.+\\.(js|jsx|mjs)$': 'babel-jest',
-  },
+  ]
 };
 
 module.exports = config;
